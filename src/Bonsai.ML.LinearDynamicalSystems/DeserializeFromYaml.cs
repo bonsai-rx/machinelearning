df4180b360e6918d457aac7bfc72b00aece6039f
Nicholas <ncguilbeault@gmail.com>
2024-01-24 11:06:37 +0000

    Remove old xml attribute

--- src/Bonsai.ML.LinearDynamicalSystems/DeserializeFromYaml.cs
+++ src/Bonsai.ML.LinearDynamicalSystems/DeserializeFromYaml.cs
@@ -10,7 +10,6 @@ namespace Bonsai.ML.LinearDynamicalSystems
     [System.Xml.Serialization.XmlIncludeAttribute(typeof(Bonsai.Expressions.TypeMapping<State>))]
     [System.Xml.Serialization.XmlIncludeAttribute(typeof(Bonsai.Expressions.TypeMapping<StateEstimate>))]
     [System.Xml.Serialization.XmlIncludeAttribute(typeof(Bonsai.Expressions.TypeMapping<EstimateWithUncertainty>))]
-    [System.Xml.Serialization.XmlIncludeAttribute(typeof(Bonsai.Expressions.TypeMapping<Ellipse>))]
     [System.ComponentModel.DescriptionAttribute("Deserializes a sequence of YAML strings into data model objects.")]
     public partial class DeserializeFromYaml : Bonsai.Expressions.SingleArgumentExpressionBuilder
     {
