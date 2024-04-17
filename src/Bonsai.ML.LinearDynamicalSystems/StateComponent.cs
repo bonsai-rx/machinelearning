55435982f0b0a4694d392a9ec3cccf02c4f70fbd
ncguilbeault <ncguilbeault@gmail.com>
2024-04-17 11:36:14 +0100

    Changed StateComponent to source node

--- src/Bonsai.ML.LinearDynamicalSystems/StateComponent.cs
+++ src/Bonsai.ML.LinearDynamicalSystems/StateComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System;
+using System.Reactive.Linq;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 
@@ -10,7 +11,7 @@ namespace Bonsai.ML.LinearDynamicalSystems
     /// </summary>
     [Description("State component of a Kalman Filter")]
     [Combinator()]
-    [WorkflowElementCategory(ElementCategory.Transform)]
+    [WorkflowElementCategory(ElementCategory.Source)]
     public class StateComponent
     {
 
@@ -63,9 +64,45 @@ namespace Bonsai.ML.LinearDynamicalSystems
             Variance = Sigma(P[i,i]);
         }
 
+        /// <summary>
+        /// Creates a new state compenent
+        /// </summary>
+        public StateComponent(double mean, double variance) 
+        {
+            Mean = mean;
+            Variance = variance;
+        }
+
         private double Sigma(double variance)
         {
             return 2 * Math.Sqrt(variance);
+        }
+
+        /// <summary>
+        /// Given an observable sequence, this function returns an observable sequence of state components for each element in the input sequence
+        /// /// </summary>
+        public IObservable<StateComponent> Process<TSource>(IObservable<TSource> source)
+        {
+            return Observable.Select(source, value =>
+            {
+                return new StateComponent (
+                    Mean = Mean,
+                    Variance = Variance
+                );
+            });
+        }
+
+        /// <summary>
+        /// This function returns an observable sequence of state components
+        /// /// </summary>
+        public IObservable<StateComponent> Process()
+        {
+            return Observable.Return(
+                new StateComponent (
+                    Mean = Mean,
+                    Variance = Variance
+                )
+            );
         }
     }
 }
