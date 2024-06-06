5a1a0bbbe3eb7cd8f04f16dc2dad8ebb41ad0c2d
ncguilbeault <ncguilbeault@gmail.com>
2024-06-06 11:00:27 +0100

    Added json serialization

---
+++ src/Bonsai.ML.HiddenMarkovModels/DeserializeFromJson.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System;
+using System.Reactive.Linq;
+using Bonsai.Expressions;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Bonsai.ML.HiddenMarkovModels
+{
+    /// <summary>
+    /// Deserializes a sequence of JSON strings into data model objects.
+    /// </summary>
+    [DefaultProperty(nameof(Type))]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [XmlInclude(typeof(TypeMapping<State>))]
+    [Description("Deserializes a sequence of JSON strings into data model objects.")]
+    public partial class DeserializeFromJson : SingleArgumentExpressionBuilder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeserializeFromJson"/> class.
+        /// </summary>
+        public DeserializeFromJson()
+        {
+            Type = new TypeMapping<State>();
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the object to deserialize.
+        /// </summary>
+        [Description("The type of the object to deserialize.")]
+        public TypeMapping Type { get; set; }
+
+        /// <inheritdoc/>
+        public override Expression Build(IEnumerable<Expression> arguments)
+        {
+            TypeMapping typeMapping = Type;
+            var returnType = typeMapping.GetType().GetGenericArguments()[0];
+            return Expression.Call(
+                typeof(DeserializeFromJson),
+                nameof(Process),
+                new Type[] { returnType },
+                Enumerable.Single(arguments));
+        }
+
+        private static IObservable<T> Process<T>(IObservable<string> source)
+        {
+            return source.Select(value => JsonConvert.DeserializeObject<T>(value));
+        }
+    }
+}
---
+++ src/Bonsai.ML.HiddenMarkovModels/SerializeToJson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Newtonsoft.Json;
+
+namespace Bonsai.ML.HiddenMarkovModels
+{
+    /// <summary>
+    /// Serializes a sequence of data model objects into JSON strings.
+    /// </summary>
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Serializes a sequence of data model objects into JSON strings.")]
+    public class SerializeToJson
+    {
+        private IObservable<string> Process<T>(IObservable<T> source)
+        {
+            return source.Select(value => JsonConvert.SerializeObject(value));
+        }
+
+        /// <summary>
+        /// Serializes each <see cref="State"/> object in the sequence to
+        /// a JSON string.
+        /// </summary>
+        /// <param name="source">
+        /// A sequence of <see cref="State"/> objects.
+        /// </param>
+        /// <returns>
+        /// A sequence of JSON strings representing the corresponding
+        /// <see cref="State"/> object.
+        /// </returns>
+        public IObservable<string> Process(IObservable<State> source)
+        {
+            return Process<State>(source);
+        }
+    }
+}
