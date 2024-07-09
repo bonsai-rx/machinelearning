00eb5724fedefb11646538829871878b7852cf1c
ncguilbeault <ncguilbeault@gmail.com>
2024-07-09 15:35:24 +0100

    Changed name of state observation clusters/statistics to gaussian observations

--- src/Bonsai.ML.HiddenMarkovModels/Observations/GaussianObservationsStatistics.cs
+++ src/Bonsai.ML.HiddenMarkovModels/Observations/GaussianObservationsStatistics.cs
@@ -5,12 +5,12 @@ using System.Reactive.Linq;
 using Python.Runtime;
 using System.Xml.Serialization;
 
-namespace Bonsai.ML.HiddenMarkovModels
+namespace Bonsai.ML.HiddenMarkovModels.Observations
 {
     [Combinator]
     [Description("")]
     [WorkflowElementCategory(ElementCategory.Transform)]
-    public class StateObservationClusters
+    public class GaussianObservationsStatistics
     {
         /// <summary>
         /// The means of the observations for each state.
@@ -18,6 +18,13 @@ namespace Bonsai.ML.HiddenMarkovModels
         [XmlIgnore]
         [Description("The means of the observations for each state.")]
         public double[,] Means { get; private set; }
+
+        /// <summary>
+        /// The standard deviations of the observations for each state.
+        /// </summary>
+        [XmlIgnore]
+        [Description("The standard deviations of the observations for each state.")]
+        public double[,] StdDevs { get; private set; }
 
         /// <summary>
         /// The covariance matrices of the observations for each state.
@@ -40,18 +47,21 @@ namespace Bonsai.ML.HiddenMarkovModels
         [Description("The sequence of inferred most probable states.")]
         public long[] InferredMostProbableStates { get; private set; }
 
-        public IObservable<StateObservationClusters> Process(IObservable<PyObject> source)
+        public IObservable<GaussianObservationsStatistics> Process(IObservable<PyObject> source)
         {
             return Observable.Select(source, pyObject =>
             {
-                var meansPyObj = (double[,])pyObject.GetArrayAttr("observation_means");
+                var observationsPyObj = pyObject.GetAttr("observations");
+                var meansPyObj = (double[,])observationsPyObj.GetArrayAttr("mus");
+                var stdDevsPyObj = (double[,])observationsPyObj.GetArrayAttr("Sigmas");
                 var covarianceMatricesPyObj = (double[,,])pyObject.GetArrayAttr("observation_covs");
                 var batchObservationsPyObj = (double[,])pyObject.GetArrayAttr("batch_observations");
                 var inferredMostProbableStatesPyObj = (long[])pyObject.GetArrayAttr("inferred_most_probable_states");
 
-                return new StateObservationClusters
+                return new GaussianObservationsStatistics
                 {
                     Means = meansPyObj,
+                    StdDevs = stdDevsPyObj,
                     CovarianceMatrices = covarianceMatricesPyObj,
                     BatchObservations = batchObservationsPyObj,
                     InferredMostProbableStates = inferredMostProbableStatesPyObj
