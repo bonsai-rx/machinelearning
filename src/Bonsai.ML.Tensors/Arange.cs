19497a535e4cd1e0e984bfe9da197e1d3db8d5a4
ncguilbeault <ncguilbeault@gmail.com>
2024-08-23 15:12:24 +0100

    Added arange function

---
+++ src/Bonsai.ML.Tensors/Arange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.Tensors
+{
+    /// <summary>
+    /// Creates a 1-D tensor of values within a given range given the start, end, and step.
+    /// </summary>
+    [Combinator]
+    [Description("Creates a 1-D tensor of values within a given range given the start, end, and step.")]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class Arange
+    {
+        /// <summary>
+        /// The start of the range.
+        /// </summary>
+        public int Start { get; set; } = 0;
+
+        /// <summary>
+        /// The end of the range.
+        /// </summary>
+        public int End { get; set; } = 10;
+
+        /// <summary>
+        /// The step of the range.
+        /// </summary>
+        public int Step { get; set; } = 1;
+
+        /// <summary>
+        /// Generates an observable sequence of 1-D tensors created with the <see cref="arange"/> function.
+        /// </summary>
+        public IObservable<Tensor> Process()
+        {
+            return Observable.Defer(() => Observable.Return(arange(Start, End, Step)));
+        }
+    }
+}
