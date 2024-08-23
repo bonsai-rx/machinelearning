f47083cda486aaa20038e4b36376e6173b5495bf
ncguilbeault <ncguilbeault@gmail.com>
2024-08-23 17:56:22 +0100

    Added ones

---
+++ src/Bonsai.ML.Tensors/Ones.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.Tensors
+{
+    /// <summary>
+    /// Creates a tensor filled with ones.
+    /// </summary>
+    [Combinator]
+    [Description("Creates a tensor filled with ones.")]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class Ones
+    {
+        /// <summary>
+        /// The size of the tensor.
+        /// </summary>
+        public long[] Size { get; set; } = [0];
+
+        /// <summary>
+        /// Generates an observable sequence of tensors filled with ones.
+        /// </summary>
+        /// <returns></returns>
+        public IObservable<Tensor> Process()
+        {
+            return Observable.Defer(() => Observable.Return(ones(Size)));
+        }
+    }
+}
