f879960dabf456826a401dc97f8ae7b708a28d4f
ncguilbeault <ncguilbeault@gmail.com>
2024-08-23 18:05:16 +0100

    Added methods to convert OpenCV types

---
+++ src/Bonsai.ML.Tensors/ToImage.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using OpenCV.Net;
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.Tensors
+{
+    /// <summary>
+    /// Converts the input tensor into an OpenCV image.
+    /// </summary>
+    [Combinator]
+    [Description("")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class ToImage
+    {
+        /// <summary>
+        /// Converts the input tensor into an OpenCV image.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public IObservable<IplImage> Process(IObservable<Tensor> source)
+        {
+            return source.Select(Helpers.OpenCVHelper.ToImage);
+        }
+    }
+}
