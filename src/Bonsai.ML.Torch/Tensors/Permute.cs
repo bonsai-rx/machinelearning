918df3a0e283f876129e89e5ea010b2c30b564b8
ncguilbeault <ncguilbeault@gmail.com>
2024-08-27 12:51:18 +0100

    Refactored to torch namespace instead of tensors

---
+++ src/Bonsai.ML.Torch/Tensors/Permute.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.Torch.Tensors
+{
+    /// <summary>
+    /// Permutes the dimensions of the input tensor according to the specified permutation.
+    /// </summary>
+    [Combinator]
+    [Description("Permutes the dimensions of the input tensor according to the specified permutation.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class Permute
+    {
+        /// <summary>
+        /// The permutation of the dimensions.
+        /// </summary>
+        public long[] Dimensions { get; set; } = [0];
+
+        /// <summary>
+        /// Returns an observable sequence that permutes the dimensions of the input tensor according to the specified permutation.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public IObservable<Tensor> Process(IObservable<Tensor> source)
+        {
+            return source.Select(tensor => {
+                return tensor.permute(Dimensions);
+            });
+        }
+    }
+}
