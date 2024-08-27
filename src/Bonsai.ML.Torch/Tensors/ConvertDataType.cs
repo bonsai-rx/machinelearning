b2ff7bc1f04555da82ced14afacf1c74ad9ee37c
ncguilbeault <ncguilbeault@gmail.com>
2024-08-27 12:51:18 +0100

    Refactored to torch namespace instead of tensors

---
+++ src/Bonsai.ML.Torch/Tensors/ConvertDataType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.Torch.Tensors
+{
+    /// <summary>
+    /// Converts the input tensor to the specified scalar type.
+    /// </summary>
+    [Combinator]
+    [Description("Converts the input tensor to the specified scalar type.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class ConvertDataType
+    {
+        /// <summary>
+        /// The scalar type to which to convert the input tensor.
+        /// </summary>
+        public ScalarType Type { get; set; } = ScalarType.Float32;
+
+        /// <summary>
+        /// Returns an observable sequence that converts the input tensor to the specified scalar type.
+        /// </summary>
+        public IObservable<Tensor> Process(IObservable<Tensor> source)
+        {
+            return source.Select(tensor =>
+            {
+                return tensor.to_type(Type);
+            });
+        }
+    }
+}
---
+++ src/Bonsai.ML.Torch/Tensors/InitializeTorchDevice.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+using TorchSharp;
+
+namespace Bonsai.ML.Torch.Tensors
+{
+    /// <summary>
+    /// Initializes the Torch device with the specified device type.
+    /// </summary>
+    [Combinator]
+    [Description("Initializes the Torch device with the specified device type.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class InitializeTorchDevice
+    {
+        /// <summary>
+        /// The device type to initialize.
+        /// </summary>
+        public DeviceType DeviceType { get; set; }
+
+        /// <summary>
+        /// Initializes the Torch device with the specified device type.
+        /// </summary>
+        /// <returns></returns>
+        public IObservable<Device> Process()
+        {
+            return Observable.Defer(() =>
+            {
+                InitializeDeviceType(DeviceType);
+                return Observable.Return(new Device(DeviceType));
+            });
+        }
+    }
+}
