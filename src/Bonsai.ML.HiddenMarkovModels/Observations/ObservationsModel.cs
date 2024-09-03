b17377e280d0ff1fa958cc0dd1095b6dffe94cdb
ncguilbeault <ncguilbeault@gmail.com>
2024-09-03 15:30:08 +0100

    Change base model classes to inherit from python model

--- src/Bonsai.ML.HiddenMarkovModels/Observations/ObservationsModel.cs
+++ src/Bonsai.ML.HiddenMarkovModels/Observations/ObservationsModel.cs
@@ -1,100 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Bonsai.ML.Python;
+using System.ComponentModel;
 
 namespace Bonsai.ML.HiddenMarkovModels.Observations
 {
     /// <summary>
-    /// An abstract class for creating an observations model.
+    /// An abstract class for creating an Observations model.
     /// </summary>
-    public abstract class ObservationsModel : PythonStringBuilder
+    public abstract class ObservationsModel : PythonModel
     {
         /// <summary>
-        /// The type of observations model.
+        /// The type of Observations model.
         /// </summary>
         public abstract ObservationsModelType ObservationsModelType { get; }
 
-        /// <summary>
-        /// The parameters that are used to define the observations models.
-        /// </summary>
-        public abstract object[] Params { get; set; }
+        /// <inheritdoc/>
+        [Browsable(false)]
+        protected override string ModelName => "observations";
 
-        /// <summary>
-        /// The keyword arguments that are used to construct the observations models.
-        /// </summary>
-        public virtual Dictionary<string, object> Kwargs => new();
+        /// <inheritdoc/>
+        [Browsable(false)]
+        protected override string ModelType => ObservationsModelLookup.GetString(ObservationsModelType);
 
-        /// <summary>
-        /// Checks the constructor parameters.
-        /// </summary>
-        /// <param name="args">The constructor parameters.</param>
-        /// <returns>True if the constructor parameters are valid; otherwise, false.</returns>
-        protected virtual bool CheckConstructorArgs(params object[] args)
+        /// <inheritdoc/>
+        public ObservationsModel() : base()
         {
-            return true;
-        }
-
-        /// <summary>
-        /// Updates the keyword arguments of the observations model.
-        /// </summary>
-        /// <param name="kwargs">The keyword arguments.</param>
-        protected virtual void UpdateKwargs(params object[] kwargs)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ObservationsModel"/> class.
-        /// </summary>
-        /// <param name="args">The constructor parameters.</param>
-        protected ObservationsModel(params object[] args)
-        {
-            CheckConstructorArgs(args);
-            UpdateKwargs(args);
-            UpdateString();
         }
 
         /// <inheritdoc/>
-        protected override string BuildString()
+        public ObservationsModel(params object[] kwargs) : base(kwargs)
         {
-            StringBuilder.Clear();
-            StringBuilder.Append($"observations_model_type=\"{ObservationsModelLookup.GetString(ObservationsModelType)}\"");
-
-            if (Params != null && Params.Length > 0) 
-            {
-                var paramsStringBuilder = new StringBuilder();
-                paramsStringBuilder.Append(",observation_params=(");
-
-                foreach (var param in Params) {
-                    if (param is null) {
-                        paramsStringBuilder.Clear();
-                        break;
-                    }
-                    var arrString = param is Array array ? NumpyHelper.NumpyParser.ParseArray(array) : param.ToString();
-                    paramsStringBuilder.Append($"{arrString},");
-                }
-
-                if (paramsStringBuilder.Length > 0) {
-                    paramsStringBuilder.Remove(paramsStringBuilder.Length - 1, 1);
-                    paramsStringBuilder.Append(")");
-                    StringBuilder.Append(paramsStringBuilder);
-                }
-            }
-
-            if (Kwargs is not null && Kwargs.Count > 0)
-            {
-                StringBuilder.Append(",observation_kwargs={");
-                foreach (var kp in Kwargs) {
-                    StringBuilder.Append($"\"{kp.Key}\":{(kp.Value is null ? "None" 
-                        : kp.Value is Array ? NumpyHelper.NumpyParser.ParseArray((Array)kp.Value)
-                        : kp.Value is string ? $"\"{kp.Value}\""
-                        : kp.Value)},");
-                }
-                StringBuilder.Remove(StringBuilder.Length - 1, 1);
-                StringBuilder.Append("}");   
-            }
-
-            return StringBuilder.ToString();
         }
     }
 }
--- src/Bonsai.ML.HiddenMarkovModels/Transitions/TransitionsModel.cs
+++ src/Bonsai.ML.HiddenMarkovModels/Transitions/TransitionsModel.cs
@@ -1,169 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using Bonsai.ML.Python;
-using System.Xml.Serialization;
-using System.Linq;
 using System.ComponentModel;
 
 namespace Bonsai.ML.HiddenMarkovModels.Transitions
 {
     /// <summary>
-    /// An abstract class for creating an Transitions model.
+    /// An abstract class for creating a Transitions model.
     /// </summary>
-    public abstract class TransitionsModel : PythonStringBuilder
+    public abstract class TransitionsModel : PythonModel
     {
         /// <summary>
         /// The type of Transitions model.
         /// </summary>
-        [Browsable(false)]
         public abstract TransitionsModelType TransitionsModelType { get; }
 
-        /// <summary>
-        /// The parameters that are used to define the Transitions models.
-        /// </summary>
+        /// <inheritdoc/>
         [Browsable(false)]
-        public virtual object[] Params 
-        { 
-            get => null; 
-            set
-            {
-                CheckParams(value);
-                UpdateParams(value);
-                UpdateString();
-            }
-        }
+        protected override string ModelName => "transitions";
 
-        /// <summary>
-        /// The array of keywords used to construct the Transitions models.
-        /// </summary>
+        /// <inheritdoc/>
         [Browsable(false)]
-        public static string[] KwargsArray => null;
+        protected override string ModelType => TransitionsModelLookup.GetString(TransitionsModelType);
 
-        /// <summary>
-        /// The dictionary of keyword arguments that are used to construct the Transitions models.
-        /// </summary>
-        [Browsable(false)]
-        [XmlIgnore]
-        public virtual Dictionary<string, object> Kwargs => new();
-
-        /// <summary>
-        /// Checks the keyword arguments.
-        /// </summary>
-        /// <param name="kwargs">The keyword arguments.</param>
-        protected virtual void CheckKwargs(params object[] kwargs)
+        /// <inheritdoc/>
+        public TransitionsModel() : base()
         {
-        }
-
-        /// <summary>
-        /// Updates the keyword arguments of the Transitions model.
-        /// </summary>
-        /// <param name="kwargs">The keyword arguments.</param>
-        protected virtual void UpdateKwargs(params object[] kwargs)
-        {
-        }
-
-        /// <summary>
-        /// Checks the parameters.
-        /// </summary>
-        /// <param name="params">The parameters.</param>
-        protected virtual void CheckParams(params object[] @params)
-        {
-        }
-
-        /// <summary>
-        /// Updates the parameters of the Transitions model.
-        /// </summary>
-        /// <param name="params">The parameters.</param>
-        protected virtual void UpdateParams(params object[] @params)
-        {
-        }
-
-        /// <summary>
-        /// Constructs a new instance of the <see cref="TransitionsModel"/> class.
-        /// </summary>
-        public TransitionsModel()
-        {
-            BuildString();
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TransitionsModel"/> class using keyword arguments.
-        /// </summary>
-        /// <param name="kwargs">The keyword arguments.</param>
-        public TransitionsModel(params object[] kwargs)
-        {
-            CheckKwargs(kwargs);
-            UpdateKwargs(kwargs);
-            UpdateString();
         }
 
         /// <inheritdoc/>
-        protected override string BuildString()
+        public TransitionsModel(params object[] kwargs) : base(kwargs)
         {
-            StringBuilder.Clear();
-            StringBuilder.Append($"transitions_model_type=\"{TransitionsModelLookup.GetString(TransitionsModelType)}\"");
-
-            if (Params != null && Params.Length > 0 && Params.All(p => p != null)) 
-            {
-                var paramsStringBuilder = new StringBuilder();
-
-                foreach (var param in Params) 
-                {
-                    if (param is null) 
-                    {
-                        paramsStringBuilder.Append("None");
-                    }
-                    else if (param is Array array) 
-                    {
-                        paramsStringBuilder.Append(NumpyHelper.NumpyParser.ParseArray(array));
-                    } 
-                    else if (param is IList list) 
-                    {
-                        if (list.Count == 0) 
-                        {
-                            paramsStringBuilder.Append("[]");
-                        }
-                        else
-                        {
-                            paramsStringBuilder.Append("[");
-                            foreach (var item in list) 
-                            {
-                                paramsStringBuilder.Append(item is Array itemArray ? NumpyHelper.NumpyParser.ParseArray(itemArray) : item.ToString());
-                                paramsStringBuilder.Append(",");
-                            }
-                            paramsStringBuilder.Remove(paramsStringBuilder.Length - 1, 1);
-                            paramsStringBuilder.Append("]");
-                        }
-                    } 
-                    else 
-                    {
-                        paramsStringBuilder.Append(param.ToString());
-                    }
-
-                    paramsStringBuilder.Append(",");
-                }
-
-                paramsStringBuilder.Remove(paramsStringBuilder.Length - 1, 1);
-                StringBuilder.Append($",transition_params=({paramsStringBuilder})");
-                
-            }
-
-            if (Kwargs is not null && Kwargs.Count > 0)
-            {
-                StringBuilder.Append(",transition_kwargs={");
-                foreach (var kp in Kwargs) {
-                    StringBuilder.Append($"\"{kp.Key}\":{(kp.Value is null ? "None" 
-                        : kp.Value is Array array ? NumpyHelper.NumpyParser.ParseArray(array)
-                        : kp.Value is string ? $"\"{kp.Value}\""
-                        : kp.Value)},");
-                }
-                StringBuilder.Remove(StringBuilder.Length - 1, 1);
-                StringBuilder.Append("}");   
-            }
-
-            return StringBuilder.ToString();
         }
     }
 }
