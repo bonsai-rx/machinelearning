9099cf958e8f5ebacabc6d2f44444aacabcd9860
ncguilbeault <ncguilbeault@gmail.com>
2024-10-16 14:47:40 +0100

    Updated to use shared module interface

--- src/Bonsai.ML.Torch/NeuralNets/LoadPretrainedModel.cs
+++ src/Bonsai.ML.Torch/NeuralNets/LoadPretrainedModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Reactive.Linq;
 using static TorchSharp.torch;
 using System.Xml.Serialization;
-using static TorchSharp.torch.nn;
 using Bonsai.Expressions;
 
 namespace Bonsai.ML.Torch.NeuralNets
@@ -18,9 +17,9 @@ namespace Bonsai.ML.Torch.NeuralNets
 
         private int numClasses = 10;
 
-        public IObservable<Module> Process()
+        public IObservable<nn.Module<Tensor, Tensor>> Process()
         {
-            Module<Tensor,Tensor> model = null;
+            nn.Module<Tensor,Tensor> model = null;
             var modelName = ModelName.ToString().ToLower();
             var device = Device;
 
