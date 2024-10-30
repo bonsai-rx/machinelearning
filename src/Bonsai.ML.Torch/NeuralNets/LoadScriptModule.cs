befe81bea79205f63a69ffde516593611745fee1
ncguilbeault <ncguilbeault@gmail.com>
2024-10-30 13:13:31 +0000

    Removed unnecessary null string

---
+++ src/Bonsai.ML.Torch/NeuralNets/LoadScriptModule.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+using System.Xml.Serialization;
+
+namespace Bonsai.ML.Torch.NeuralNets
+{
+    [Combinator]
+    [Description("")]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class LoadScriptModule
+    {
+
+        [XmlIgnore]
+        public Device Device { get; set; } = CPU;
+
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        public string ModelPath { get; set; }
+
+        public IObservable<nn.IModule<Tensor, Tensor>> Process()
+        {
+            return Observable.Return((nn.IModule<Tensor, Tensor>)jit.load<Tensor, Tensor>(ModelPath, Device));
+        }
+    }
+}
