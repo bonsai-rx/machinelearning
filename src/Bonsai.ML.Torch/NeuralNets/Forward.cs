f76181e29c8f459a46df10fcfe8d5071f4c131e2
ncguilbeault <ncguilbeault@gmail.com>
2024-10-30 13:12:54 +0000

    Modified to use Module interface

---
+++ src/Bonsai.ML.Torch/NeuralNets/Forward.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+using System.Xml.Serialization;
+using TorchSharp.Modules;
+
+namespace Bonsai.ML.Torch.NeuralNets
+{
+    [Combinator]
+    [Description("")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class Forward
+    {
+        [XmlIgnore]
+        public nn.Module<Tensor, Tensor> Model { get; set; }
+
+        public IObservable<Tensor> Process(IObservable<Tensor> source)
+        {
+            return source.Select(Model.forward);
+        }
+    }
+}
