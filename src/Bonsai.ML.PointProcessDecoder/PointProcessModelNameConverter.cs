3a68d6b6cbaabd521b0d145cea4a7296376edeee
ncguilbeault <ncguilbeault@gmail.com>
2025-01-10 11:58:37 +0000

    Added point process model package

---
+++ src/Bonsai.ML.PointProcessDecoder/PointProcessModelNameConverter.cs
@@ -0,0 +1,43 @@
+using Bonsai;
+using Bonsai.Expressions;
+using System.Linq;
+using System.ComponentModel;
+
+namespace Bonsai.ML.PointProcessDecoder;
+
+/// <summary>
+/// Provides a type converter to display a list of available point process models.
+/// </summary>
+public class PointProcessModelNameConverter : StringConverter
+{
+    /// <inheritdoc/>
+    public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+    {
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+    {
+        if (context != null)
+        {
+            var workflowBuilder = (WorkflowBuilder)context.GetService(typeof(WorkflowBuilder));
+            if (workflowBuilder != null)
+            {
+                var models = (from builder in workflowBuilder.Workflow.Descendants()
+                                    where builder.GetType() != typeof(DisableBuilder)
+                                    let createPointProcessModel = ExpressionBuilder.GetWorkflowElement(builder) as CreatePointProcessModel
+                                    where createPointProcessModel != null && !string.IsNullOrEmpty(createPointProcessModel.Name)
+                                    select createPointProcessModel.Name)
+                                    .Distinct()
+                                    .ToList();
+                if (models.Count > 0)
+                {
+                    return new StandardValuesCollection(models);
+                }
+            }
+        }
+
+        return new StandardValuesCollection(new string[] { });
+    }
+}
