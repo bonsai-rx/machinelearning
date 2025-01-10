8831bec588f0a7d0f3e64560031bce1c50944430
ncguilbeault <ncguilbeault@gmail.com>
2025-01-10 11:58:37 +0000

    Added point process model package

---
+++ src/Bonsai.ML.PointProcessDecoder/Encode.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.PointProcessDecoder;
+
+/// <summary>
+/// Encodes the combined state observation data and neural data into a point process model.
+/// </summary>
+[Combinator]
+[WorkflowElementCategory(ElementCategory.Sink)]
+[Description("Encodes the combined state observation data and neural data into a point process model.")]
+public class Encode
+{
+    /// <summary>
+    /// The name of the point process model to use.
+    /// </summary>
+    [TypeConverter(typeof(PointProcessModelNameConverter))]
+    public string Model { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Encodes the combined state observation data and neural data into a point process model.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tuple<Tensor, Tensor>> Process(IObservable<Tuple<Tensor, Tensor>> source)
+    {
+        return source.Do(input =>
+        {
+            var model = PointProcessModelManager.GetModel(Model);
+            var (neuralData, stateObservations) = input;
+            model.Encode(neuralData, stateObservations);
+        });
+    }
+}
