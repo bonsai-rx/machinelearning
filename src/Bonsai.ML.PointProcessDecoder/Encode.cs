92ff9f09665f265053e85d3d6857f7549cd96cc0
ncguilbeault <ncguilbeault@gmail.com>
2025-01-28 16:13:48 +0000

    Added property descriptions

--- src/Bonsai.ML.PointProcessDecoder/Encode.cs
+++ src/Bonsai.ML.PointProcessDecoder/Encode.cs
@@ -18,6 +18,7 @@ public class Encode
     /// The name of the point process model to use.
     /// </summary>
     [TypeConverter(typeof(PointProcessModelNameConverter))]
+    [Description("The name of the point process model to use.")]
     public string Model { get; set; } = string.Empty;
 
     /// <summary>
