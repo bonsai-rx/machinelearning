517826ce7a6769b0ad5377100f1ca2f5fa599fb4
ncguilbeault <ncguilbeault@gmail.com>
2025-03-13 12:45:18 +0000

    Added additional documentation to `ScalarTypeConverter`

--- src/Bonsai.ML.Torch/ScalarTypeConverter.cs
+++ src/Bonsai.ML.Torch/ScalarTypeConverter.cs
@@ -21,6 +21,9 @@ namespace Bonsai.ML.Torch
                 return true;
             if (sourceType == typeof(Type))
                 return true;
+            if (sourceType == typeof(ScalarType))
+                return true;
+
             return base.CanConvertFrom(context, sourceType);
         }
 
@@ -38,12 +41,20 @@ namespace Bonsai.ML.Torch
         }
 
         /// <inheritdoc/>
+        // We currently only support a subset of all possible ScalarTypes when converting to/from .NET types using the ScalarTypeLookup class.
+        // Properties of type `ScalarType` that use this converter will only display the ScalarTypes that are currently supported for conversion.
+        // Those properties will display the ScalarType enum value represented in string format.
+        // Properties of type `Type` that use this converter will display the .NET types that are currently supported.
+        // These properties will use the corresponding string value in the lookup table for display.
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
+            // Get the property type we are converting from
             var propType = context?.PropertyDescriptor?.PropertyType;
 
+            // Check if we are converting from a string or Type to ScalarType
             if (propType == typeof(ScalarType))
             {
+                // Convert from string to ScalarType
                 if (value is string stringValue)
                 {
                     foreach (var scalarType in ScalarTypeLookup.ScalarTypes)
@@ -53,7 +64,9 @@ namespace Bonsai.ML.Torch
                     }
                     throw new ArgumentException($"'{value}' is not a supported ScalarType.");
                 }
-                if (value is Type type)
+
+                // Convert from Type to ScalarType
+                else if (value is Type type)
                 {
                     if (ScalarTypeLookup.TryGetScalarTypeFromType(type, out var found))
                         return found;
@@ -61,15 +74,19 @@ namespace Bonsai.ML.Torch
                 }
             }
 
+            // Check if we are converting from a string or ScalarType to Type            
             else if (propType == typeof(Type))
             {
+                // Convert from string to Type
                 if (value is string stringValue)
                 {
                     if (ScalarTypeLookup.TryGetScalarTypeFromString(stringValue, out var found))
                         return ScalarTypeLookup.GetTypeFromScalarType(found);
                     throw new ArgumentException($"'{value}' is not a supported Type.");
                 }
-                if (value is ScalarType scalarType)
+
+                // Convert from ScalarType to Type
+                else if (value is ScalarType scalarType)
                 {
                     if (ScalarTypeLookup.TryGetTypeFromScalarType(scalarType, out var found))
                         return found;
@@ -81,6 +98,9 @@ namespace Bonsai.ML.Torch
         }
 
         /// <inheritdoc/>
+        // When converting to a designated type, we will convert the value based on whether the property type is `ScalarType` or `Type`.
+        // If the property type is `ScalarType`, we will convert the value to a string representation of the ScalarType enum.
+        // If the property type is `Type`, we will convert the value to the corresponding string representation in the lookup table.
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
             var propType = context?.PropertyDescriptor?.PropertyType;
@@ -128,6 +148,7 @@ namespace Bonsai.ML.Torch
         public override bool GetStandardValuesExclusive(ITypeDescriptorContext context) => true;
 
         /// <inheritdoc/>
+        // We will return a collection of supported ScalarTypes if the property type is `ScalarType` or a collection of supported .NET types if the property type is `Type`.
         public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
         {
             if (context?.PropertyDescriptor?.PropertyType == typeof(ScalarType))
