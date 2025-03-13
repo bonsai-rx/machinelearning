ac57e49ea71ff86b921d80a3050cd3b2457e31a1
ncguilbeault <ncguilbeault@gmail.com>
2025-03-13 12:47:29 +0000

    Refactored `Set` class to simplify lambda expressions and added overloads to accept additional tensor/index combinations in input sequence

--- src/Bonsai.ML.Torch/Set.cs
+++ src/Bonsai.ML.Torch/Set.cs
@@ -1,15 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Xml.Serialization;
+using TorchSharp;
 using static TorchSharp.torch;
 
 namespace Bonsai.ML.Torch;
 
 /// <summary>
-/// Sets the value of the input tensor at the specified index.
+/// Sets values of a tensor to the provided values at the specified index.
 /// </summary>
 [Combinator]
 [Description("Sets the value of the input tensor at the specified index.")]
@@ -30,43 +30,62 @@ public class Set
     public Tensor Value { get; set; } = null;
 
     /// <summary>
-    /// Returns an observable sequence that sets the value of the input tensor at the specified index.
+    /// Returns an observable sequence that sets the value of the input tensor to the `Value` property at indices specified by the `Index` property.
     /// </summary>
     /// <param name="source"></param>
     /// <returns></returns>
     public IObservable<Tensor> Process(IObservable<Tensor> source)
     {
-        return source.Select(tensor => {
-            var indexes = Torch.Index.IndexHelper.Parse(Index);
-            return tensor.index_put_(Value, indexes);
-        });
+        return source.Select(tensor => tensor.index_put_(Value, IndexHelper.Parse(Index)));
     }
 
     /// <summary>
-    /// Returns an observable sequence that sets the value of the input tensor at the specified index.
+    /// Returns an observable sequence that sets the value of the input tensor to the `Value` property using the tensor index in the input sequence.
     /// </summary>
     /// <param name="source"></param>
     /// <returns></returns>
-    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, TensorIndex>> source)
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, torch.TensorIndex>> source)
     {
-        return source.Select(input => {
-            var tensor = input.Item1;
-            var index = input.Item2;
-            return tensor.index_put_(Value, index);
-        });
+        return source.Select(input => input.Item1.index_put_(Value, input.Item2));
     }
 
     /// <summary>
-    /// Returns an observable sequence that sets the value of the input tensor at the specified index.
+    /// Returns an observable sequence that sets the value of the input tensor to the `Value` property using the tensor index array in the input sequence.
     /// </summary>
     /// <param name="source"></param>
     /// <returns></returns>
-    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, TensorIndex[]>> source)
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, torch.TensorIndex[]>> source)
     {
-        return source.Select(input => {
-            var tensor = input.Item1;
-            var indexes = input.Item2;
-            return tensor.index_put_(Value, indexes);
-        });
+        return source.Select(input => input.Item1.index_put_(Value, input.Item2));
+    }
+
+    /// <summary>
+    /// Returns an observable sequence that sets the value of the input tensor in item 1 to item 2 at indices specified by the `Index` property.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, Tensor>> source)
+    {
+        return source.Select(input => input.Item1.index_put_(input.Item2, IndexHelper.Parse(Index)));
+    }
+
+    /// <summary>
+    /// Returns an observable sequence that sets the value of the input tensor in item 1 to item 2 at the tensor index specified in item 3.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, Tensor, torch.TensorIndex>> source)
+    {
+        return source.Select(input => input.Item1.index_put_(input.Item2, input.Item3));
+    }
+
+    /// <summary>
+    /// Returns an observable sequence that sets the value of the input tensor in item 1 to item 2 at the tensor index array specified in item 3.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, Tensor, torch.TensorIndex[]>> source)
+    {
+        return source.Select(input => input.Item1.index_put_(input.Item2, input.Item3));
     }
 }
