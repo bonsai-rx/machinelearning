9e72aab7b43aec40aa23f7b36ecfee82f66adda6
ncguilbeault <ncguilbeault@gmail.com>
2025-03-13 12:50:40 +0000

    Add `SignLogDeterminant` class to compute `slogdet`

---
+++ src/Bonsai.ML.Torch/LinearAlgebra/SignLogDeterminant.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+
+namespace Bonsai.ML.Torch.LinearAlgebra
+{
+    /// <summary>
+    /// Computes the determinant of a square matrix.
+    /// </summary>
+    [Combinator]
+    [Description("Computes the sign and natural logarithm of the absolute value of the determinant of a square matrix.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class SignLogDeterminant
+    {
+        /// <summary>
+        /// <summary>
+        /// Computes the determinant of a square matrix.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public IObservable<(Tensor, Tensor)> Process(IObservable<Tensor> source)
+        {
+            return source.Select(linalg.slogdet);
+        }
+    }
+}
