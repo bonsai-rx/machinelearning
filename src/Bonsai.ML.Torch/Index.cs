31f5481a6c985ff51f3d3afbeeab9d896e884efc
ncguilbeault <ncguilbeault@gmail.com>
2025-03-21 11:12:40 +0000

    Added overloads to the Process method of `Index` for indexing tensors with single and multiple indices.

--- src/Bonsai.ML.Torch/Index.cs
+++ src/Bonsai.ML.Torch/Index.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Reactive.Linq;
+using TorchSharp;
 using static TorchSharp.torch;
 
 namespace Bonsai.ML.Torch;
@@ -29,5 +32,25 @@ public class Index
     {
         var index = IndexHelper.Parse(Value);
         return source.Select(tensor =>  tensor.index(index));
+    }
+
+    /// <summary>
+    /// Indexes the input tensor with the specified indices.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, torch.TensorIndex>> source)
+    {
+        return source.Select(value => value.Item1.index(new torch.TensorIndex[] { value.Item2 }));
+    }
+
+    /// <summary>
+    /// Indexes the input tensor with the specified indices.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, IEnumerable<torch.TensorIndex>>> source)
+    {
+        return source.Select(value => value.Item1.index(value.Item2.ToArray()));
     }
 }
