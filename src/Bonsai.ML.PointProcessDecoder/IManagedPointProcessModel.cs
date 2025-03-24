41b44b2d94bfa102b00e507e330deca6f32bccf0
ncguilbeault <ncguilbeault@gmail.com>
2025-03-24 13:18:04 +0000

    Create interface called `IManagedPointProcessModel` for classes which share access to managed models

---
+++ src/Bonsai.ML.PointProcessDecoder/IManagedPointProcessModel.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Represents a node that uses a managed point process model.
+/// </summary>
+public interface IManagedPointProcessModelNode
+{
+    /// <summary>
+    /// Gets the name of the point process model.
+    /// </summary>
+    string Name { get; }
+}
