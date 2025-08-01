b9661655b23fae223c01db06872ae6085eef55c3
ncguilbeault <ncguilbeault@gmail.com>
2025-08-01 18:37:51 +0100

    Added a class for seeding a torch compatible RNG

--- src/Bonsai.ML.Torch/Generator.cs
+++ src/Bonsai.ML.Torch/Generator.cs
@@ -3,12 +3,14 @@ using static TorchSharp.torch;
 using TorchSharp;
 using System;
 using System.Reactive.Linq;
+using System.Xml.Serialization;
 
 namespace Bonsai.ML.Torch;
 
 [Combinator]
 public class Generator
 {
+    [XmlIgnore]
     public Device Device { get; set; }
 
     public ulong Seed { get; set; } = 0;
