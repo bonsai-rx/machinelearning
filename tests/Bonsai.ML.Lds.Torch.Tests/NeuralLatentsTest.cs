a326d09e84d27b77df37baa1809d8806b2a42bb6
ncguilbeault <ncguilbeault@gmail.com>
2025-10-23 18:24:59 +0100

    Modified test case to download data and only run Bonsai script as opposed to running both the Python and Bonsai code to save memory

--- tests/Bonsai.ML.Lds.Torch.Tests/NeuralLatentsTest.cs
+++ tests/Bonsai.ML.Lds.Torch.Tests/NeuralLatentsTest.cs
@@ -21,71 +21,28 @@ public class NeuralLatentsTest
 {
     private readonly string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
 
-    private static void RunPythonScript(string basePath)
+    private static void DownloadData(string basePath)
     {
-        var pythonExec = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? "python"
-            : "python3";
-        var scriptPath = Path.Combine(basePath, "bootstrap_test_environment.py");
-        ProcessHelper.RunProcess(pythonExec, $"\"{scriptPath}\" {basePath}");
+        string zipFileUrl = "https://zenodo.org/records/17427805/files/Bonsai.ML.Lds.Torch.Tests.zip";
 
-        Console.WriteLine("Run python script finished.");
-    }
-
-    private static double[] ReadBinaryFile(string fileName)
-    {
-        Console.WriteLine($"Reading binary file: {fileName}");
-        using var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-        using var binaryReader = new BinaryReader(fileStream);
-        var fileLength = fileStream.Length;
-        var numDoubles = fileLength / sizeof(double);
-        var data = new double[numDoubles];
-        for (int i = 0; i < numDoubles; i++)
+        try
         {
-            data[i] = binaryReader.ReadDouble();
+            byte[] responseBytes;
+            using (var httpClient = new HttpClient())
+            {
+                responseBytes = httpClient.GetByteArrayAsync(zipFileUrl).Result;
+                Console.WriteLine("File downloaded successfully.");
+            }
+
+            using MemoryStream zipStream = new(responseBytes);
+            using ZipArchive zip = new(zipStream, ZipArchiveMode.Read);
+            zip.ExtractToDirectory(basePath);
+            Console.WriteLine("File extracted successfully.");
         }
-        Console.WriteLine($"Read {numDoubles} doubles from {fileName}");
-        return data;
-    }
-
-    private static void WriteToTensor(string fileName, long[] shape)
-    {
-        Console.WriteLine($"Reading filename: {fileName} and creating tensor with shape [{string.Join(", ", shape)}]");
-        var data = ReadBinaryFile(fileName);
-        var tensor = from_array(data).reshape(shape);
-        var outputFileName = Path.ChangeExtension(fileName, ".pt");
-        tensor.Save(outputFileName);
-        Console.WriteLine($"Saved tensor to {outputFileName}");
-    }
-
-    private static void ConvertBinaryFiles(string basePath)
-    {
-        var transformedBinnedSpikesFileName = Path.Combine(basePath, "transformed_binned_spikes.bin");
-        WriteToTensor(transformedBinnedSpikesFileName, [142, -1]);
-
-        var transitionMatrixFileName = Path.Combine(basePath, "python_B0.bin");
-        WriteToTensor(transitionMatrixFileName, [10, 10]);
-
-        var measurementFunctionFileName = Path.Combine(basePath, "python_Z0.bin");
-        WriteToTensor(measurementFunctionFileName, [142, 10]);
-
-        var processNoiseFileName = Path.Combine(basePath, "python_Q0.bin");
-        WriteToTensor(processNoiseFileName, [10, 10]);
-
-        var observationNoiseFileName = Path.Combine(basePath, "python_R0.bin");
-        WriteToTensor(observationNoiseFileName, [142, 142]);
-
-        var initialStateFileName = Path.Combine(basePath, "python_m0_0.bin");
-        WriteToTensor(initialStateFileName, [10]);
-
-        var initialCovarianceFileName = Path.Combine(basePath, "python_V0_0.bin");
-        WriteToTensor(initialCovarianceFileName, [10, 10]);
-
-        var outputMeansFileName = Path.Combine(basePath, "python_means.bin");
-        WriteToTensor(outputMeansFileName, [10, -1]);
-
-        var outputCovariancesFileName = Path.Combine(basePath, "python_covs.bin");
-        WriteToTensor(outputCovariancesFileName, [10, 10, -1]);
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
     }
 
     private static async Task RunBonsaiWorkflow(string basePath)
@@ -108,14 +65,11 @@ public class NeuralLatentsTest
     /// Setup for the test.
     /// </summary>
     [TestInitialize]
-    [DeploymentItem("bootstrap_test_environment.py")]
-    [DeploymentItem("estimate_neural_latents.py")]
     [DeploymentItem("NeuralLatentsTest.bonsai")]
     public async Task TestSetup()
     {
         Directory.CreateDirectory(basePath);
-        RunPythonScript(basePath);
-        ConvertBinaryFiles(basePath);
+        DownloadData(basePath);
         await RunBonsaiWorkflow(basePath);
     }
 
@@ -126,21 +80,9 @@ public class NeuralLatentsTest
     public void TestCleanup()
     {
         var ptFiles = Directory.GetFiles(basePath, "*.pt");
-        var binFiles = Directory.GetFiles(basePath, "*.bin");
+        var zipFiles = Directory.GetFiles(basePath, "*.zip");
         foreach (var file in ptFiles) File.Delete(file);
-        foreach (var file in binFiles) File.Delete(file);
-
-        var virtualEnvPath = Path.Combine(basePath, ".venv");
-        if (Directory.Exists(virtualEnvPath))
-        {
-            Directory.Delete(virtualEnvPath, true);
-        }
-
-        var remfileCachePath = Path.Combine(basePath, "remfile_cache");
-        if (Directory.Exists(remfileCachePath))
-        {
-            Directory.Delete(remfileCachePath, true);
-        }
+        foreach (var file in zipFiles) File.Delete(file);
     }
 
     /// <summary>
