49bc1b225697c673ea8dcd48783b2b131e901518
ncguilbeault <ncguilbeault@gmail.com>
2025-10-23 13:12:51 +0100

    Updated bootstrapping Python script to support multiple args to pip install command

--- tests/Bonsai.ML.Lds.Python.Tests/bootstrap_test_environment.py
+++ tests/Bonsai.ML.Lds.Python.Tests/bootstrap_test_environment.py
@@ -11,6 +11,14 @@ def get_base_dir(base_dir = None):
         return os.path.dirname(os.path.realpath(__file__))
     except:
         return os.getcwd()
+    
+def get_pip_path(venv_path: str = None):
+    if venv_path is None:
+        venv_path = get_venv_path()
+    if sys.platform.startswith('linux'):
+        return os.path.join(venv_path, 'bin', 'pip')
+    else:
+        return os.path.join(venv_path, 'Scripts', 'pip.exe')
 
 def create_venv(parent_dir = None):
     # function to create a virtual environment
@@ -33,15 +41,14 @@ def activate_venv(venv_path = None):
         os.environ["PATH"] = os.pathsep.join([bin_path, *os.environ.get("PATH", "").split(os.pathsep)])
         sys.path.insert(0, os.path.join(venv_path, 'Lib', 'site-packages'))
 
-def install(package, venv_path = None):
+def install(venv_path: str = None, pip_args: list[str] = None):
     # function to install pip packages into a virtual environment
     if venv_path is None:
-        venv_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '.venv')
-    if sys.platform.startswith('linux'):
-        pip_path = os.path.join(venv_path, 'bin', 'pip')
-    else:
-        pip_path = os.path.join(venv_path, 'Scripts', 'pip.exe')
-    subprocess.check_call([pip_path, "install", package])
+        venv_path = get_venv_path()
+    pip_path = get_pip_path(venv_path)
+    if pip_args is None:
+        raise ValueError("pip_args must be provided")
+    subprocess.check_call([pip_path, "install", *pip_args])
 
 parser = argparse.ArgumentParser()
 parser.add_argument("base_dir", type=str, default=None)
@@ -51,8 +58,8 @@ args = parser.parse_args()
 base_dir = get_base_dir(args.base_dir)
 venv_path = create_venv(base_dir)
 activate_venv(venv_path)
-install("pandas", venv_path)
-install("lds_python@git+https://github.com/ncguilbeault/lds_python@dc7a2e02892033734746bc0615dc294f5b43f672", venv_path)
+install(venv_path, ["--no-cache-dir", "pandas"])
+install(venv_path, ["--no-cache-dir", "lds_python@git+https://github.com/ncguilbeault/lds_python@dc7a2e02892033734746bc0615dc294f5b43f672"])
 
 if sys.platform.startswith('linux'):
     python_path = os.path.join(venv_path, "bin", "python")
