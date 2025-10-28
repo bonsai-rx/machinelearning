2b5c5ac9e8cffa2c27eeb4421c6831750ff8e0a4
ncguilbeault <ncguilbeault@gmail.com>
2025-10-28 19:26:42 +0000

    Refactored `SaveKalmanFilterParameters` class to save to a folder rather than individual files which is more consistent with other `Bonsai.ML` packages

--- src/Bonsai.ML.Lds.Torch/SaveKalmanFilterParameters.cs
+++ src/Bonsai.ML.Lds.Torch/SaveKalmanFilterParameters.cs
@@ -3,8 +3,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Xml.Serialization;
-using Bonsai.ML.Torch;
 using System.IO;
+using Bonsai.ML.Torch;
 using TorchSharp;
 using static TorchSharp.torch;
 
@@ -19,75 +19,122 @@ namespace Bonsai.ML.Lds.Torch;
 public class SaveKalmanFilterParameters
 {
     /// <summary>
-    /// Specifies the path to use for saving the transition matrix of a Kalman filter to a .bin file.
+    /// The path to the folder where the Kalman filter model parameters will be saved.
     /// </summary>
-    [Description("Specifies the path to use for saving the transition matrix of a Kalman filter to a .bin file.")]
-    [FileNameFilter("Binary Data (*.bin)|*.bin|All Files|*.*")]
-    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string TransitionMatrixFilePath { get; set; } = "transition_matrix.bin";
+    [Editor("Bonsai.Design.FolderNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+    [Description("The path to the folder where the Kalman filter model parameters will be saved.")]
+    public string Path { get; set; } = string.Empty;
 
     /// <summary>
-    /// Specifies the path to use for saving the measurement function of a Kalman filter to a .bin file.
+    /// If true, the contents of the folder will be overwritten if it already exists.
     /// </summary>
-    [Description("Specifies the path to use for saving the measurement function of a Kalman filter to a .bin file.")]
-    [FileNameFilter("Binary Data (*.bin)|*.bin|All Files|*.*")]
-    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string MeasurementFunctionFilePath { get; set; } = "measurement_function.bin";
+    [Description("If true, the contents of the folder will be overwritten if it already exists.")]
+    public bool Overwrite { get; set; } = false;
 
     /// <summary>
-    /// Specifies the path to use for saving the process noise covariance of a Kalman filter to a .bin file.
+    /// Specifies the type of suffix to add to the save path.
+    /// The suffix is added as a subfolder to the save path.
+    /// If DateTime is used, a suffix with the current date and time is added to the save path in the format, '[Path]/yyyyMMddHHmmss/...'.
+    /// If Guid is used, a suffix with a unique 128-bit identifier is added to the save path in the format, '[Path]/[128-bit identifier]/...'.
     /// </summary>
-    [Description("Specifies the path to use for saving the process noise covariance of a Kalman filter to a .bin file.")]
-    [FileNameFilter("Binary Data (*.bin)|*.bin|All Files|*.*")]
-    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string ProcessNoiseCovarianceFilePath { get; set; } = "process_noise_covariance.bin";
+    [Description("Specifies the type of suffix to add to the save path.")]
+    public SuffixType AddSuffix { get; set; } = SuffixType.None;
 
-    /// <summary>
-    /// Specifies the path to use for saving the measurement noise covariance of a Kalman filter to a .bin file.
-    /// </summary>
-    [Description("Specifies the path to use for saving the measurement noise covariance of a Kalman filter to a .bin file.")]
-    [FileNameFilter("Binary Data (*.bin)|*.bin|All Files|*.*")]
-    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string MeasurementNoiseCovarianceFilePath { get; set; } = "measurement_noise_covariance.bin";
-
-    /// <summary>
-    /// Specifies the path to use for saving the initial mean of a Kalman filter to a .bin file.
-    /// </summary>
-    [Description("Specifies the path to use for saving the initial mean of a Kalman filter to a .bin file.")]
-    [FileNameFilter("Binary Data (*.bin)|*.bin|All Files|*.*")]
-    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string InitialMeanFilePath { get; set; } = "initial_mean.bin";
-
-    /// <summary>
-    /// Specifies the path to use for saving the initial covariance of a Kalman filter to a .bin file.
-    /// </summary>
-    [Description("Specifies the path to use for saving the initial covariance of a Kalman filter to a .bin file.")]
-    [FileNameFilter("Binary Data (*.bin)|*.bin|All Files|*.*")]
-    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string InitialCovarianceFilePath { get; set; } = "initial_covariance.bin";
-
-    private void SaveTensorToFile(Tensor tensor, string filePath)
+    private void SaveKalmanFilterParametersToDisk(KalmanFilterParameters parameters)
     {
-        if (filePath != null)
+        if (string.IsNullOrEmpty(Path))
         {
-            tensor.Save(filePath);
+            Path = Directory.GetCurrentDirectory();
         }
+
+        var path = AddSuffix switch
+        {
+            SuffixType.DateTime => System.IO.Path.Combine(Path, $"{HighResolutionScheduler.Now:yyyyMMddHHmmss}"),
+            SuffixType.Guid => System.IO.Path.Combine(Path, $"{Guid.NewGuid()}"),
+            _ => Path
+        };
+
+        var transitionMatrixPath = System.IO.Path.Combine(path, "TransitionMatrix.bin");
+        var measurementFunctionPath = System.IO.Path.Combine(path, "MeasurementFunction.bin");
+        var processNoiseCovariancePath = System.IO.Path.Combine(path, "ProcessNoiseCovariance.bin");
+        var measurementNoiseCovariancePath = System.IO.Path.Combine(path, "MeasurementNoiseCovariance.bin");
+        var initialMeanPath = System.IO.Path.Combine(path, "InitialMean.bin");
+        var initialCovariancePath = System.IO.Path.Combine(path, "InitialCovariance.bin");
+
+        if (Directory.Exists(path))
+        {
+            if (!Overwrite && (
+                File.Exists(transitionMatrixPath) ||
+                File.Exists(measurementFunctionPath) ||
+                File.Exists(processNoiseCovariancePath) ||
+                File.Exists(measurementNoiseCovariancePath) ||
+                File.Exists(initialMeanPath) ||
+                File.Exists(initialCovariancePath))
+            )
+            {
+                throw new InvalidOperationException("The save path already exists.");
+            }
+            else
+            {
+                if (File.Exists(transitionMatrixPath))
+                    File.Delete(transitionMatrixPath);
+                if (File.Exists(measurementFunctionPath))
+                    File.Delete(measurementFunctionPath);
+                if (File.Exists(processNoiseCovariancePath))
+                    File.Delete(processNoiseCovariancePath);
+                if (File.Exists(measurementNoiseCovariancePath))
+                    File.Delete(measurementNoiseCovariancePath);
+                if (File.Exists(initialMeanPath))
+                    File.Delete(initialMeanPath);
+                if (File.Exists(initialCovariancePath))
+                    File.Delete(initialCovariancePath);
+            }
+        }
+
+        Directory.CreateDirectory(path);
+
+        parameters.TransitionMatrix.Save(transitionMatrixPath);
+        parameters.MeasurementFunction.Save(measurementFunctionPath);
+        parameters.ProcessNoiseCovariance.Save(processNoiseCovariancePath);
+        parameters.MeasurementNoiseCovariance.Save(measurementNoiseCovariancePath);
+        parameters.InitialMean.Save(initialMeanPath);
+        parameters.InitialCovariance.Save(initialCovariancePath);
     }
 
     /// <summary>
-    /// Creates parameters for a Kalman filter model using the properties of this class.
+    /// Processes an observable sequence of Kalman filter parameters, saving to files.
+    /// </summary>
+    public IObservable<KalmanFilterParameters> Process(IObservable<KalmanFilterParameters> source)
+    {
+        return source.Do(SaveKalmanFilterParametersToDisk);
+    }
+
+    /// <summary>
+    /// Processes an observable sequence of Kalman filter models, saving their parameters to files.
     /// </summary>
     public IObservable<KalmanFilter> Process(IObservable<KalmanFilter> source)
     {
-        return source.Do(model =>
-        {
-            var parameters = model.Parameters;
-            SaveTensorToFile(parameters.TransitionMatrix, TransitionMatrixFilePath);
-            SaveTensorToFile(parameters.MeasurementFunction, MeasurementFunctionFilePath);
-            SaveTensorToFile(parameters.ProcessNoiseCovariance, ProcessNoiseCovarianceFilePath);
-            SaveTensorToFile(parameters.MeasurementNoiseCovariance, MeasurementNoiseCovarianceFilePath);
-            SaveTensorToFile(parameters.InitialMean, InitialMeanFilePath);
-            SaveTensorToFile(parameters.InitialCovariance, InitialCovarianceFilePath);                
-        });
+        return source.Do(model => SaveKalmanFilterParametersToDisk(model.Parameters));
+    }
+
+    /// <summary>
+    /// Specifies the type of suffix to add to the save path.
+    /// </summary>
+    public enum SuffixType
+    {
+        /// <summary>
+        /// No suffix is added to the save path.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// A suffix with the current date and time is added to the save path.
+        /// </summary>
+        DateTime,
+
+        /// <summary>
+        /// A suffix with a unique identifier is added to the save path.
+        /// </summary>
+        Guid
     }
 }
