9be41b867256a0c4a2a8275ac2a9ecbf73ffd149
ncguilbeault <ncguilbeault@gmail.com>
2025-11-10 14:55:57 +0000

    Added XML docs to classes

--- src/Bonsai.ML.Torch/PrintTensor.cs
+++ src/Bonsai.ML.Torch/PrintTensor.cs
@@ -9,12 +9,25 @@ using static TorchSharp.torch;
 
 namespace Bonsai.ML.Torch;
 
+/// <summary>
+/// Prints the string representation of incoming tensors to the console.
+/// </summary>
 [Combinator]
-[Description("")]
+[Description("Prints the string representation of incoming tensors to the console.")]
 [WorkflowElementCategory(ElementCategory.Sink)]
 public class PrintTensor
 {
+    /// <summary>
+    /// Gets or sets the string style used to format the tensor output.
+    /// </summary>
+    [Description("The string style used to format the tensor output.")]
     public TensorStringStyle StringStyle { get; set; }
+
+    /// <summary>
+    /// Processes the input sequence of tensors and prints their string representations to the console.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
     public IObservable<Tensor> Process(IObservable<Tensor> source)
     {
         return source.Do(value => Console.WriteLine(value.ToString(StringStyle)));
