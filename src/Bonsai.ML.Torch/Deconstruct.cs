26900fd92b03055780768b9be555b737b79f51cd
ncguilbeault <ncguilbeault@gmail.com>
2025-11-11 13:36:57 +0000

    Renamed `Decompose` to `Deconstruct` for better alignment with operators true function (splitting along dimension)

--- src/Bonsai.ML.Torch/Deconstruct.cs
+++ src/Bonsai.ML.Torch/Deconstruct.cs
@@ -7,18 +7,18 @@ using TorchSharp;
 namespace Bonsai.ML.Torch;
 
 /// <summary>
-/// This operator decomposes each incoming tensor into a sequence of tensors by splitting it along the specified dimension.
+/// This operator deconstructs each incoming tensor into a sequence of tensors by splitting it along the specified dimension.
 /// </summary>
 [Combinator]
-[Description("Decomposes each incoming tensor into a sequence of tensors by splitting it along the specified dimension.")]
+[Description("Deconstructs each incoming tensor into a sequence of tensors by splitting it along the specified dimension.")]
 [WorkflowElementCategory(ElementCategory.Combinator)]
-public class Decompose
+public class Deconstruct
 {
     private int _dimension = 0;
     /// <summary>
-    /// Gets or sets the dimension along which to split the tensor.
+    /// Gets or sets the dimension along which to deconstruct the tensor.
     /// </summary>
-    [Description("The dimension along which to split the tensor.")]
+    [Description("The dimension along which to deconstruct the tensor.")]
     public int Dimension
     {
         get => _dimension;
@@ -26,7 +26,7 @@ public class Decompose
     }
 
     /// <summary>
-    /// Processes an observable sequence of tensors, decomposing each tensor into a sequence of tensors by splitting along the specified dimension.
+    /// Processes an observable sequence of tensors, deconstructing each tensor into a sequence of tensors by splitting along the specified dimension.
     /// </summary>
     public IObservable<torch.Tensor> Process(IObservable<torch.Tensor> source)
     {
