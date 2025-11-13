26e1dc3832505bdd66162d6eba5b1a373a60890e
ncguilbeault <ncguilbeault@gmail.com>
2025-11-13 18:04:53 +0000

    Added operator for performing `CrossProduct`

---
+++ src/Bonsai.ML.Torch/LinearAlgebra/CrossProduct.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using static TorchSharp.torch;
+using static TorchSharp.torch.linalg;
+
+namespace Bonsai.ML.Torch.LinearAlgebra;
+
+/// <summary>
+/// Computes the cross product of 2 tensors.
+/// </summary>
+[Combinator]
+[Description("Computes the cross product of 2 tensors.")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class CrossProduct
+{
+    /// <summary>
+    /// The dimension to perform the operation.
+    /// </summary>
+    public long Dimension { get; set; } = -1;
+
+    /// <summary>
+    /// Computes the cross product of 2 tensors.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Tensor> Process(IObservable<Tuple<Tensor, Tensor>> source)
+    {
+        return source.Select(value =>
+        {
+            return cross(value.Item1, value.Item2, Dimension);
+        });
+    }
+}
