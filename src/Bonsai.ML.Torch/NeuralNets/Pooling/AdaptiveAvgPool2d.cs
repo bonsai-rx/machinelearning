b3e7b25a0f9058e85dbd7388e7d6c077c675955f
ncguilbeault <ncguilbeault@gmail.com>
2025-12-04 15:36:37 +0000

    Added pooling module builder

---
+++ src/Bonsai.ML.Torch/NeuralNets/Pooling/AdaptiveAvgPool2d.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Xml.Serialization;
+using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+namespace Bonsai.ML.Torch.NeuralNets.Pooling;
+
+/// <summary>
+/// Represents an operator that creates a 2D adaptive average pooling layer.
+/// </summary>
+[Description("Creates a 2D adaptive average pooling layer.")]
+public class AdaptiveAvgPool2d
+{
+    /// <summary>
+    /// The output size.
+    /// </summary>
+    [Description("The output size")]
+    [TypeConverter(typeof(UnidimensionalArrayConverter))]
+    public long[] OutputSize { get; set; }
+
+    /// <summary>
+    /// Creates an AdaptiveAvgPool2d module.
+    /// </summary>
+    public IObservable<Module<Tensor, Tensor>> Process()
+    {
+        return Observable.Return(AdaptiveAvgPool2d(OutputSize));
+    }
+
+    /// <summary>
+    /// Creates an AdaptiveAvgPool2d module.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Module<Tensor, Tensor>> Process<T>(IObservable<T> source)
+    {
+        return source.Select(_ => AdaptiveAvgPool2d(OutputSize));
+    }
+}
---
+++ src/Bonsai.ML.Torch/NeuralNets/Pooling/MaxPool3d.cs
@@ -0,0 +1,67 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Xml.Serialization;
+using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+namespace Bonsai.ML.Torch.NeuralNets.Pooling;
+
+/// <summary>
+/// Represents an operator that creates a 3D max pooling layer.
+/// </summary>
+[Description("Creates a 3D max pooling layer.")]
+public class MaxPool3d
+{
+    /// <summary>
+    /// The kernel size.
+    /// </summary>
+    [Description("The kernel size.")]
+    public long[] KernelSize { get; set; }
+
+    /// <summary>
+    /// The stride.
+    /// </summary>
+    [Description("The stride.")]
+    public long[] Stride { get; set; } = null;
+
+    /// <summary>
+    /// The padding.
+    /// </summary>
+    [Description("The padding.")]
+    public long[] Padding { get; set; } = null;
+
+    /// <summary>
+    /// The dilation.
+    /// </summary>
+    [Description("The dilation.")]
+    public long[] Dilation { get; set; } = null;
+
+    /// <summary>
+    /// The ceiling mode.
+    /// </summary>
+    [Description("The ceiling mode.")]
+    public bool CeilMode { get; set; } = false;
+
+    /// <summary>
+    /// Creates an MaxPool3d module.
+    /// </summary>
+    public IObservable<Module<Tensor, Tensor>> Process()
+    {
+        return Observable.Return(MaxPool3d(KernelSize, Stride, Padding, Dilation, CeilMode));
+    }
+
+    /// <summary>
+    /// Creates an MaxPool3d module.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public IObservable<Module<Tensor, Tensor>> Process<T>(IObservable<T> source)
+    {
+        return source.Select(_ => MaxPool3d(KernelSize, Stride, Padding, Dilation, CeilMode));
+    }
+}
